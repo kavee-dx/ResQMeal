084f6b1ea0281ce8b68010908614ea20b127b7d7
Amasha Kavindi Bollagala <amashakav23@gmail.com>
2026-08-09 22:25:04 +0530

    Added text input to register page

--- frontend/src/screens/RegisterScreen.tsx
+++ frontend/src/screens/RegisterScreen.tsx
@@ -1,15 +1,15 @@
 import React from "react";
-import {View, Text} from "react-native";
+import { View, Text, TextInput } from "react-native";
 
-
-export default function RegisterScreen(){
-
-    return(
-        <View>
-            <Text>
-                Register Screen
-            </Text>
-        </View>
-    )
-
+export default function RegisterScreen() {
+  return (
+    <View>
+      <Text>Register Screen</Text>
+      <TextInput
+        placeholder="Email"
+        keyboardType="email-address"
+        className="px-4 py-4 mb-4 text-gray-800 bg-gray-100 rounded-xl"
+      />
+    </View>
+  );
 }
